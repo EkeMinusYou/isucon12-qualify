38587b0c3d83c4ee74ab3679d9fe14e4914f4123
EkeMinusYou <euonymuslke@gmail.com>
2023-10-17 03:02:31 +0900

    enable go profile

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = pprof.StartCPUProfile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go func() {
+			sig := make(chan os.Signal, 1)
+			signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+			<-sig
+			pprof.StopCPUProfile()
+			f.Close()
+			os.Exit(0)
+		}()
+	}
+
 	Run()
 }
